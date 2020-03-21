b619d1938bcb40db1dd825208c39b2b7adf148a7
Petra Setlíková <st55592@upce.cz>
2020-03-21 16:57:41 +0100

    Rozepsano VybudujPodstrom v RozsahovemStromu

--- Datove-struktury-2020/Data/RozsahovyStrom.cs
+++ Datove-struktury-2020/Data/RozsahovyStrom.cs
@@ -4,35 +4,95 @@ using System.Text;
 
 namespace Datove_struktury_2020.Data
 {
-    class RozsahovyStrom<T> 
+    class RozsahovyStrom<T> where T : ISouradnice
     {
+
         PrvekRozsahovehoStromu koren;
-        
-        public void Vybuduj() 
-        { 
-           
-        }
-        private void VybudujStrom() { }
-        private void VybudujPodstrom() { }
-        public T Najdi() 
+        int pocetPrvkuVeStrukture;
+        int pocetUrovniStromu;
+        //delegát porovnani - reseno CompareTo
+
+       
+        public void Vybuduj(List<T> seznamPrvku) 
         {
+            if (seznamPrvku == null || seznamPrvku.Count == 0)
+            {
+                throw new Exception("Strukturu Rozsahový strom nešlo vybudovat, list vrcholů je prázdný nebo neexistuje.");
+            }
+            else
+            {
+                pocetPrvkuVeStrukture = seznamPrvku.Count;
+                //vybudovani stromu prvni dimenze
+                koren = VybudujStrom(seznamPrvku, null, true);
+            }
+        }
+        // dimenzeX - aby bylo zřejmé, jestli se bude budovat od dimenze X nebo Y
+        // prvekDruheDimenze - asi aby se odkazal Y interval do Xoveho intervalu
+        private PrvekRozsahovehoStromu VybudujStrom(List<T> seznamPrvku, PrvekRozsahovehoStromu prvekDruheDimenze, bool dimenzeX) 
+        {
+            PrvekRozsahovehoStromu pomocny = VybudujPodstrom(seznamPrvku, null, null, dimenzeX);
+            //případné spojení vrcholu podstromu s prvkem opačné dimenze
+            if (prvekDruheDimenze != null)
+            {
+                pomocny.druhaDimenze = prvekDruheDimenze;
+            }
+            return pomocny;
+        }
+        private PrvekRozsahovehoStromu VybudujPodstrom(List<T> seznamPrvku, PrvekRozsahovehoStromu predek,
+            PrvekRozsahovehoStromu predchoziZListu, bool dimenzeX)
+        {  
+            PrvekRozsahovehoStromu pomocny;
+            if (seznamPrvku.Count >= 2)
+            {
+                if (dimenzeX == true)
+                {  
+                    // seznamPrvku.Sort(Comparison<in T ())
+                }
+            }
+
+            //dodelat!
+            return predek;
+        }
+        public T Najdi(ISouradnice obeSouradnice) 
+        {
+
             return default(T); 
         }
-        public List<T> NajdiInterval() 
+        /// <summary>
+        /// VErejna implementace pro Intervalove vyhledavani. Vola privatni metodu NajdiInterval, kde je implementovano vyledavani.
+        /// </summary>
+        /// <param name="levyHorniBod">Levý horní roh v oblasti intervalového vyhledávání.</param>
+        /// <param name="pravyDolniBod">Pravý horní roh v oblasti intervalového vyhledávání.</param>
+        /// <returns>Vracé seznam vrcholů ve vybraném intervalu. </returns>
+        public List<T> NajdiInterval(ISouradnice levyHorniBod, ISouradnice pravyDolniBod) 
         {
-            return new List<T> ();    
+            //dle diplomky se pri prvnim volani nastavi vrchol jako koren a dimenzeX na true
+            return NajdiInterval(levyHorniBod, pravyDolniBod,koren,true);    
         }
 
+        private List<T> NajdiInterval(ISouradnice levyHorniBod, ISouradnice pravyDolniBod, PrvekRozsahovehoStromu vrchol, bool dimenzeX)
+        {
+
+            return new List<T>();
+        }
+
+        /// <summary>
+        /// Uzel stromu
+        /// </summary>
         private class PrvekRozsahovehoStromu
         {
-            PrvekRozsahovehoStromu levyPotomek, pravyPotomek, otec, druhaDimenze;
-            PrvekRozsahovehoStromu predchozi, dalsi;
-            bool platny = false;
-            ISouradnice data;
+            public PrvekRozsahovehoStromu levyPotomek, pravyPotomek, otec, druhaDimenze;
+            public PrvekRozsahovehoStromu predchozi, dalsi;
+            public bool platny = false;
+            public ISouradnice data;
 
-            public PrvekRozsahovehoStromu(T x, T y) 
+            public PrvekRozsahovehoStromu()
             {
-                
+            }
+
+            public PrvekRozsahovehoStromu(ISouradnice s) 
+            {
+                data = s;
             }
         }
 
