5b25b16bb02207cdcdb30287dd0c1b30237474a7
Petra Setlíková <st55592@upce.cz>
2020-03-22 21:38:54 +0100

    komentáře Rozsahový strom

--- Datove-struktury-2020/Data/RozsahovyStrom.cs
+++ Datove-struktury-2020/Data/RozsahovyStrom.cs
@@ -4,23 +4,57 @@ using System.Text;
 
 namespace Datove_struktury_2020.Data
 {
+    /// <summary>
+    /// Datová struktura tzv. Range strom. Plnohodnotné prvky se nacházejí pouze
+    /// na úrovni listů, kde jsou mezi sebou zřetězeny a zároveň seřazeny podle jedné z dimenzí.
+    /// Ostatní vrcholy stromu tvoří navigační strukturu a mají uloženu informaci o intervalu, do
+    /// kterého patří všichni jejich potomci.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     class RozsahovyStrom<T> where T : ISouradnice
     {
         PrvekRozsahovehoStromu koren;
         int pocetPrvkuVeStrukture;
         int pocetUrovniStromu;
+
         //delegát porovnani - reseno CompareTo
 
-        // podle List<T>.Sort Method, Comparison<T> Delegate
+
+
+       
+
+        /// <summary>
+        /// Porovnání součadnic v navigačním vrcholu podle X.
+        /// Využívá metodu porovnejČísla.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         static int porovnejPodleX(T a, T b)
         {
             return porovnejCisla(a.vratX(), b.vratX());
         }
+
+        /// <summary>
+        /// Porovnání součadnic v navigačním vrcholu podle Y.
+        /// Využívá metodu porovnejČísla.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         static int porovnejPodleY(T a, T b)
         {
             return porovnejCisla(a.vratY(), b.vratY());
         }
 
+        /// <summary>
+        /// Porovnání - delegát podle List<T>.Sort Method, Comparison<T> Delegate.
+        /// </summary>
+        /// <param name="hodnotaA">int</param>
+        /// <param name="hodnotaB">int</param>
+        /// <returns>Vrací int buď 0, 1, -1, podle toho, jestli jsou hodnoty stejné nebo ne. 
+        /// Když jsou stejné vrací 0. 
+        /// Když je první porovnávaná hodnota větší vrací 1.</returns>
         static int porovnejCisla(int hodnotaA, int hodnotaB)
         {
             if (hodnotaA == hodnotaB)
@@ -37,6 +71,15 @@ namespace Datove_struktury_2020.Data
             }
         }
 
+        /// <summary>
+        /// Postupně buduje rozsahovy strom. Využívá k tomu metodu Vybuduj Strom a metodu Vybuduj podstrom.
+        /// Pokud je předáné více než jeden prvek (seznamPrvků) vzniká tzv. navigační vrchol hlavního stromu.
+        /// Za první dimenzi je v realizaci zvolena souřadnice x, do navigačního se předá nejmenší možný interval se všema prvkama.
+        /// Ty se následně seřadí podle první dimenze a rozdělí se na poloviny.První polovina bude tvořit 
+        /// levý podstrom navigačního vrcholu, druhá polovina pravý podstrom. Toto se opakuje dokud nezbyde jeden poslední prvek, 
+        /// ten se pak stane liste, kde se zřetězí s dalšíma prvkama.
+        /// </summary>
+        /// <param name="seznamPrvku"></param>
         public void Vybuduj(List<T> seznamPrvku)
         {
             if (seznamPrvku == null || seznamPrvku.Count == 0)
@@ -50,8 +93,15 @@ namespace Datove_struktury_2020.Data
                 koren = VybudujStrom(seznamPrvku, null, true);
             }
         }
-        // dimenzeX - aby bylo zřejmé, jestli se bude budovat od dimenze X nebo Y
-        // prvekDruheDimenze - asi aby se odkazal Y interval do Xoveho intervalu
+
+        /// <summary>
+        ///  Pomocná metoda pro postupné budování rozsahového stromu. 
+        /// </summary>
+        /// <param name="seznamPrvku">Poskytne seznam prvků, které vstupují so stromu.</param>
+        /// <param name="prvekDruheDimenze">prvekDruheDimenze, aby se odkázal Ypsylonovy interval do Xoveho intervalu.</param>
+        /// <param name="dimenzeX">Parametr, aby bylo zřejmé, jestli se bude budovat od dimenze X nebo Y.</param>
+        /// <returns>Vrací "pomocny", což je pomocný abstraktní uzel, který drží prvek rozsahového stromu,
+        /// ať už jde o navigační vrchol nebo plnohodnotný prvek</returns>
         private PrvekRozsahovehoStromu VybudujStrom(List<T> seznamPrvku, PrvekRozsahovehoStromu prvekDruheDimenze, bool dimenzeX)
         {
             PrvekRozsahovehoStromu pomocny = VybudujPodstrom(seznamPrvku, null, null, dimenzeX);
@@ -62,6 +112,16 @@ namespace Datove_struktury_2020.Data
             }
             return pomocny;
         }
+
+        /// <summary>
+        /// Postupně vybudovává podstrom v rozsahovém stromu. Pro seřazení prvků využívá v listu metodu sort. 
+        /// </summary>
+        /// <param name="seznamPrvku">Poskytne seznam prvků, které vstupují so stromu.</param>
+        /// <param name="predek">Prvek rozsahového stromu.</param>
+        /// <param name="predchoziZListu">Předcházející hodnota ze seznamu prvků, které se vkládají postupně do stromu.</param>
+        /// <param name="dimenzeX">Parametr, aby bylo zřejmé, jestli se bude budovat od dimenze X nebo Y.</param>
+        /// <returns>Vrací "pomocny", což je pomocný abstraktní uzel, který drží prvek rozsahového stromu,
+        /// ať už jde o navigační vrchol nebo plnohodnotný prvek</returns>
         private PrvekRozsahovehoStromu VybudujPodstrom(List<T> seznamPrvku, PrvekRozsahovehoStromu predek,
             PrvekRozsahovehoStromu predchoziZListu, bool dimenzeX)
         {
@@ -122,6 +182,18 @@ namespace Datove_struktury_2020.Data
             pomocny.otec = predek;
             return pomocny;
         }
+
+        /// <summary>
+        /// Hledání prvku se zadanými souřadnicemi probíhá pouze ve stromu první dimenze, který je
+        /// organizován podle zeměpisné šířky.Prohledávání začíná od kořene stromu směrem
+        /// k listům.V každém navigačním vrcholu se testuje, zda interval vrcholu obsahuje hledanou
+        /// hodnotu zeměpisné šířky. V případě záporné odpovědi hledání skončí, protože lze
+        /// jednoznačně určit, že hledaný prvek se v range stromu nenachází. V opačném případě se
+        /// pokračuje do obou podstromů aktuálního vrcholu. Při dosažení plnohodnotného prvku
+        /// (listu stromu) se porovnají jeho souřadnice se souřadnicemi hledanými. --text z literatury-- 
+        /// </summary>
+        /// <param name="obeSouradnice">Proměnná uchovávající obě souřadnice.</param>
+        /// <returns>Prvek Rozsahového stromu </returns>
         public T Najdi(ISouradnice obeSouradnice)
         {
             // je-li kořen platným vrcholem, porovnají se souřadnice a hledání skončí
@@ -185,11 +257,8 @@ namespace Datove_struktury_2020.Data
                     }
                 }
             }
-
-
-            // v opačném případě je strom prohledáván traverzováním směrem k listům
-
         }
+
         /// <summary>
         /// Verejna implementace pro Intervalove vyhledavani. Vola privatni metodu NajdiInterval, kde je implementovano vyledavani.
         /// </summary>
@@ -202,13 +271,25 @@ namespace Datove_struktury_2020.Data
             return NajdiInterval(levyHorniBod, pravyDolniBod, koren, true);
         }
 
+        /// <summary>
+        /// Pro vyhledání prvků v obdélníkovém segmentu. Hledání opět začíná od kořene stromu směrem k listům. Při
+        /// dosažení plnohodnotného prvku(listu stromu) se zkontrolují jeho souřadnice, a pokud
+        /// spadají do hledaného segmentu, vykoná se na aktuálním prvku zadaná akce.Při
+        /// traverzování přes navigační vrcholy se rozhoduje, jakým způsobem v hledání pokračovat.
+        /// Uplatňují se přitom daná pravidla.
+        /// </summary>
+        /// <param name="levyHorniBod"></param>
+        /// <param name="pravyDolniBod"></param>
+        /// <param name="vrchol"></param>
+        /// <param name="dimenzeX"></param>
+        /// <returns></returns>
         private List<T> NajdiInterval(ISouradnice levyHorniBod, ISouradnice pravyDolniBod, PrvekRozsahovehoStromu vrchol, bool dimenzeX)
         {
             return new List<T>();
         }
 
         /// <summary>
-        /// Uzel stromu
+        /// Uzel stromu.
         /// </summary>
         private class PrvekRozsahovehoStromu
         {
